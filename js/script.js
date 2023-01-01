7244d2f3b3adbff7f0844009b532e22a8cba52b4
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-01-01 22:10:59 +0100

    Add player move to script.js

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ if (randomNumber == '1') {
 }
 printMessage('Ruch komputera: ' + computerMove);
 
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'norzyce';
+} else {
+  playerMove = 'zły wybór, wybrano kamień (1)';
+  playerInput = 1;
+}
+printMessage('Twój ruch: ' + playerMove);
+
