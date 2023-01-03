3654565679345de247f620a30d0a6d28fee32083
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-01-04 00:12:25 +0100

    Add result to script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
 
-var argButtonName, button_scissors, buttonPaper, buttonRock, buttonScissors, buttonTest;
+var argButtonName, button_scissors, buttonPaper, buttonRock, buttonScissors, buttonTest, numberRund, numberPlayer, numberPlayer;
+
+numberRund = 0; 
+numberPlayer = 0;
+numberComputer = 0;
 
 /**
  * Describe this function...
@@ -9,6 +13,7 @@ function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
   
+  
   var computerMove, playerInput, playerMove, randomNumber;
   
   /**
@@ -16,6 +21,7 @@ function buttonClicked(argButtonName) {
    */
   function getMoveName(argMoveId) {
     console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+    numberRund = numberRund + 1; 
     if (argMoveId == 1) {
       return 'kamień';
     } else if (argMoveId == 2) {
@@ -33,18 +39,25 @@ function buttonClicked(argButtonName) {
    */
   function displayResult(argPlayerMove, argComputerMove) {
     console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+ 
     if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
       printMessage('Wygrywasz!');
+      numberPlayer = numberPlayer + 1;
     } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
       printMessage('Wygrywasz!');
+      numberPlayer = numberPlayer + 1;
     } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
       printMessage('Wygrywasz!');
+      numberPlayer = numberPlayer + 1;
     } else if (argComputerMove == argPlayerMove) {
       printMessage('Remis');
     } else {
       printMessage('Przegrywasz :(');
+      numberComputer = numberComputer + 1;
     }
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    printMessage('Runda: ' + numberRund);
+    printMessage('Komputer (' + numberComputer + ' : ' + numberPlayer + ') Ty')
   }
   playerMove = argButtonName;
   console.log('wybór ruchu gracza to: ' + playerInput);
@@ -63,3 +76,4 @@ buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
+
